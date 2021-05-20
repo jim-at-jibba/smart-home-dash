b94cde6f6729629a4dc83a640e588d79e5a216e2
James Best <jim@justjibba.net>
2021-05-20 16:46:18 +0100

    Update output type

--- backup-database.sh
+++ backup-database.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 # ./backup.sh acme acme_db acme_user acme_pass
+# https://stackoverflow.com/questions/24718706/backup-restore-a-dockerized-postgresql-database
+
 APP=$1
 
 BUCKET_NAME=home-dash-db-backup
 
 TIMESTAMP=$(date +%F_%T | tr ':' '-')
 TEMP_FILE=$(mktemp tmp.XXXXXXXXXX)
-S3_FILE="s3://$BUCKET_NAME/$APP/$APP-backup-$TIMESTAMP.pgdump"
+S3_FILE="s3://$BUCKET_NAME/$APP/$APP-backup-$TIMESTAMP.sql"
 
-docker exec -t postgres pg_dump -c -U -U home > $TEMP_FILE
+docker exec -t postgres pg_dumpall -c -U -U home > $TEMP_FILE
 s3cmd put $TEMP_FILE $S3_FILE --encrypt
 rm "$TEMP_FILE"
